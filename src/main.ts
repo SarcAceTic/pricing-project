edbe96365c3f45fd0a196d537a08b2991396cd22
SarcAceTic <sarcacetic@gmail.com>
2023-11-18 13:43:43 +0700

    Installed Vuetify and Material Design icons

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,34 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import "@mdi/font/css/materialdesignicons.css";
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    },
+  },
+  theme: {
+    defaultTheme: "dark",
+  },
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
