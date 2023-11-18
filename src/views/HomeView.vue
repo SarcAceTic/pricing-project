b008356b90e0e2d5afeee801827d2251ef811964
SarcAceTic <sarcacetic@gmail.com>
2023-11-18 22:39:53 +0700

    Added toggle system to display Monthly and Annual option subscription

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,94 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const isEnabled = ref(false)
+</script>
 
 <template>
   <main>
     <p class="text-center mt-10 bod-title">Our Pricing</p>
-    <v-container class="mt-10 h-50">
-      <v-row no-gutters justify="center">
+    <v-switch class="switch-center mt-4" v-model="isEnabled">
+      <template v-slot:label>
+        <span class="switch-label">Annual</span>
+      </template>
+    </v-switch>
+    <v-container class="mt-6 h-50">
+      <!-- Yearly Prices -->
+      <v-row no-gutters justify="center" v-if="isEnabled">
+        <v-col sm="12" md="6" lg="4">
+          <v-card class="pa-3 ma-3 mx-8" color="grey-darken-4">
+            <v-card-title class="text-center">Basic</v-card-title>
+            <p class="text-h4 text-center font-weight-bold my-10 pricing-text">$199.99</p>
+            <v-divider class="mb-6"></v-divider>
+            <v-card-text class="text-center">
+              <div class="mb-4">
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">500 GB Storage</span>
+              </div>
+              <div class="mb-4">
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">2 Users Allowed</span>
+              </div>
+              <div>
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">Send up to 3 GB</span>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="grey-darken-4" variant="flat" block>Learn More</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="12" md="6" lg="4">
+          <v-card class="pa-3 ma-3 mx-8" color="purple-darken-4">
+            <v-card-title class="text-center">Professional</v-card-title>
+            <p class="text-h4 text-center font-weight-bold my-10 pricing-text">$249.99</p>
+            <v-divider class="mb-6"></v-divider>
+            <v-card-text class="text-center">
+              <div class="mb-4">
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">1 TB Storage</span>
+              </div>
+              <div class="mb-4">
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">5 Users Allowed</span>
+              </div>
+              <div>
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">Send up to 10 GB</span>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="deep-purple-darken-1" variant="flat" block>Learn More</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="12" md="6" lg="4">
+          <v-card class="pa-3 ma-3 mx-8" color="grey-darken-4">
+            <v-card-title class="text-center">Master</v-card-title>
+            <p class="text-h4 text-center font-weight-bold my-10 pricing-text">$399.99</p>
+            <v-divider class="mb-6"></v-divider>
+            <v-card-text class="text-center">
+              <div class="mb-4">
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">2 TB Storage</span>
+              </div>
+              <div class="mb-4">
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">10 Users Allowed</span>
+              </div>
+              <div>
+                <v-icon class="ma-2" color="green-accent-4">mdi-star-four-points</v-icon
+                ><span class="text-body-1">Send up to 20 GB</span>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="grey-darken-4" variant="flat" block>Learn More</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <!-- Monthly Prices -->
+      <v-row no-gutters justify="center" v-if="!isEnabled">
         <v-col sm="12" md="6" lg="4">
           <v-card class="pa-3 ma-3 mx-8" color="grey-darken-4">
             <v-card-title class="text-center">Basic</v-card-title>
@@ -91,6 +175,13 @@
   font-size: 70px !important;
 }
 .bod-title {
-  font-size: 30px !important;
+  font-size: 40px !important;
+}
+.switch-center {
+  display: flex;
+  justify-content: center;
+}
+.switch-label {
+  font-size: 20px !important;
 }
 </style>
